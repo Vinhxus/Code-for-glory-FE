09ab81ee35611b904eb17bb89f577f0f25ab4ab6
Hoang Quoc Vinh <vinh.hoangvinhhochay@hcmut.edu.vn>
2026-04-20 09:34:40 +0700

    initial commit FE

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main className="App"> 
+      <header className="App-header">
+        <h1>Hello World!</h1>
+      </header>
+    </main>
+  )
+}
+
+export default App
