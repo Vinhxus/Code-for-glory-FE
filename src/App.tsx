8f4e54390bfb0701edf3e58bcd225a08fe048509
Hoang Quoc Vinh <vinh.hoangvinhhochay@hcmut.edu.vn>
2026-04-24 19:24:37 +0700

    add router and add Tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ function App() {
   return (
     <main className="App"> 
       <header className="App-header">
-        <h1>Hello World!</h1>
+        <h1 className="text-3xl font-bold text-red-500">Hello World</h1>
       </header>
     </main>
+
+    
   )
 }
 
