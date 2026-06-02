11d357b511d27936812334c1b8fd08ec3182ec4f
Hoang Quoc Vinh <vinh.hoangvinhhochay@hcmut.edu.vn>
2026-06-02 23:20:30 +0700

    feat: adjust route

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,11 @@ import RegisterPage from './feature/auth/registerPage';
 import LoginPage from './feature/auth/loginPage';
 import Tracking from './feature/history/Tracking';
 import TabComponent from './feature/history';
+import { useAuth } from './feature/auth/useAuth';
 
 // App.tsx
 function App() {
+  const { isAuthenticated } = useAuth();
   return (
     <BrowserRouter>
       <Routes>
@@ -26,7 +28,15 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
         <Route path="/home" element={<Navigate to="/" replace />} />
-
+        <Route index element={<Navigate to="/login" replace />} />{' '}
+        {/* redirect root là login page */}
+        <Route
+          path="/login"
+          element={
+            isAuthenticated ? <Navigate to="/" replace /> : <LoginPage />
+          }
+        />{' '}
+        {/* login rồi thì không vào lại login */}
         {/* Protected routes */}
         <Route element={<ProtectedRoute />}>
           <Route path="/" element={<Homepage />} />
