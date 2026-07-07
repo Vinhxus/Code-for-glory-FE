1cdae168cf947fe958010d70e6c1f12a23369deb
Hoang Quoc Vinh <vinh.hoangvinhhochay@hcmut.edu.vn>
2026-07-07 23:17:44 +0700

    feat: add sign in by GG

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './feature/auth/authProvider';
 import './index.css';
 import App from './App.tsx';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+          <App />
+        </GoogleOAuthProvider>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
